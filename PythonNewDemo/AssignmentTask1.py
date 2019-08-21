e151d243f7d35f878ffac1bc6162ef84be939561
saurabhtatasteel <saurabh_tatasteel@yahoo.com>
2019-08-21 23:37:10 +0530

    Add project files.

---
+++ PythonNewDemo/AssignmentTask1.py
@@ -0,0 +1,45 @@
+#Given a tuple with no and str
+#if no divide by 3 and print
+#if str print second letter
+
+#len of tuple
+#loop in len
+#condition if no divide by 3 and print
+#condition else str print second letter
+
+#myInput = ('physics', 1998, 'chemistry', 1997, 2000,'I');
+
+#for x in input:
+    #if str(x).isnumeric():
+       # print(x/3)
+    #else:
+       # if len(x)== 1:
+            #print(x)
+        #else:
+           # print(x[1])
+           # print(x[1:2])
+
+fo = open("DBDemo.py" ,"r")
+str = fo.read();
+print ("Name of the file: ", fo.name)
+print("Data read : ",str)
+fo.close()
+#class Person:
+#  def __init__(self, name, age):
+#    self.name = name
+#    self.age = age
+
+#p1 = Person("John", 36)
+#p2 = Person("Shubhra", 28)
+
+#print(p1.name)
+#print(p1.age)
+
+#print(p2.name)
+#print(p2.age)
+
+
+
+
+
+
---
+++ PythonNewDemo/PythonNewDemo.py
@@ -0,0 +1,35 @@
+
+for x in "banana":
+  print(x)
+
+  fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+  if x == "banana":
+    break
+
+print("")
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+print("continue demo")
+#python
+for x in "python":    
+   if x=="h":
+       continue   
+   print(x)
+
+
+x = 8
+
+x/=3
+
+print(x)
+
+        
+
+
