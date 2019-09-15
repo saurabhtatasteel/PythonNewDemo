8edae37d38d2095cbe3edd1eb267f5fd4c95c869
saurabhtatasteel <46168743+saurabhtatasteel@users.noreply.github.com>
2019-09-15 14:22:58 +0530

    Update Notepad.py
    Testing editing

--- PythonNewDemo/TKinterDemos/Notepad.py
+++ PythonNewDemo/TKinterDemos/Notepad.py
@@ -21,6 +21,7 @@ class MyNotepad:
             openFile.close()
             mytextbox.delete('1.0', END)
             mytextbox.insert(tk.END, myfileData)
+            #testing editing
             myWindow.title(openedFileName)
 
         def SaveFile(mytextbox):
